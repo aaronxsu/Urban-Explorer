42a2cce902f989dc495895bbc16bd707d6339ee4
Xunze Su <xunzesu@Xunzes-MacBook-Pro.local>
2017-04-28 01:03:58 -0400

    Enable email sending in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #default url options
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://urban-explorer.herokuapp.com/' }
 
   #exception notifier settings
   Rails.application.config.middleware.use ExceptionNotification::Rack,
